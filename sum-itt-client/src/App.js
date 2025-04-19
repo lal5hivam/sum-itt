e6c79cddacd261b42608b19e9f7ba96e75d1b697
lal5hivam <l.singhshivam1@gmail.com>
2025-04-19 19:51:52 +0530

    Added Flashcard Generator

--- sum-itt-client/src/App.js
+++ sum-itt-client/src/App.js
@@ -8,6 +8,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ChatBotWidget from "./components/ChatBotWidget";
 import TalkWithNotes from './pages/TalkWithNotes';
+import FlashcardGenerator from './pages/FlashcardGenerator';
 
 
 import { motion } from "framer-motion";
